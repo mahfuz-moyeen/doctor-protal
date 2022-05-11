244f895861b3e4432210d249e4cb93df08eca6c7
mahfuzmoyeen10 <mahfuzzm10@gamil.com>
2022-05-11 19:19:37 +0600

    add banner

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import About from './components/pages/About/About';
+import Appointment from './components/pages/Appointment/Appointment';
+import ContactUs from './components/pages/ContactUs/ContactUs';
 import Home from './components/pages/Home/Home';
+import Login from './components/pages/Login/Login';
+import Reviews from './components/pages/Reviews/Reviews';
 import Menubar from './components/Shared/Menubar/Menubar'
 
 function App() {
@@ -9,7 +13,11 @@ function App() {
       <Menubar />
       <Routes>
         <Route path='/' element={<Home />} />
+        <Route path='/appointment' element={<Appointment />} />
+        <Route path='/reviews' element={<Reviews />} />
+        <Route path='/contact-us' element={<ContactUs />} />
         <Route path='/about' element={<About />} />
+        <Route path='/login' element={<Login />} />
       </Routes>
     </div>
   );
